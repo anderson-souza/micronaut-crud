a01a93c5242d430d67105e3c423a388710466b7d
Anderson Souza <andersonpds14@gmail.com>
2021-08-17 12:39:56 -0300

    Adiciona teste de arquitetura para evitar lançar exceções genéricas.

--- src/test/java/com/aps/architecture/CodingStandardsTest.java
+++ src/test/java/com/aps/architecture/CodingStandardsTest.java
@@ -9,8 +9,9 @@ import org.junit.jupiter.api.Test;
 import static com.aps.architecture.PackagesDefinition.DEFAULT_PACKAGE;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 import static com.tngtech.archunit.library.GeneralCodingRules.ACCESS_STANDARD_STREAMS;
+import static com.tngtech.archunit.library.GeneralCodingRules.THROW_GENERIC_EXCEPTIONS;
 
-class CodingStandartsTest {
+class CodingStandardsTest {
 
     private JavaClasses importedClasses;
 
@@ -24,6 +25,14 @@ class CodingStandartsTest {
         ArchRule rule = noClasses().should(ACCESS_STANDARD_STREAMS)
                 .because("Não se deve utilizar println para registros de log e debug. " +
                         "Utilizar sistema de logs específico para isso");
+
+        rule.check(importedClasses);
+    }
+
+    @Test
+    void classes_should_not_throw_generic_exceptions() {
+        ArchRule rule = noClasses().should(THROW_GENERIC_EXCEPTIONS)
+                .because("Classes não devem lançar exceções genéricas. Criar uma exceção específica para retorno");
 
         rule.check(importedClasses);
     }
